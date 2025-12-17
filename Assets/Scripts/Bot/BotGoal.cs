ed21668d9da64796527fd87d6504fb7d3232809b
Filep Leonardo <filep.leonardo@gmail.com>
2025-12-18 00:25:02 +0100

    Fix map1 and bot jumping by adjusting jump graph character width, collider points, land vector check and refine jump vector correction

--- Assets/Scripts/Bot/BotGoal.cs
+++ Assets/Scripts/Bot/BotGoal.cs
@@ -3,20 +3,18 @@ using UnityEngine;
 public readonly struct BotGoal
 {
     public readonly BotGoalType GoalType;
-    public readonly Vector2 AttackVector;
-    public readonly StandingPoint Destination;
+    public readonly Vector2 AimVector;
     public readonly Item PreferredItem;
 
-    public static BotGoal SkipAction() => new BotGoal(BotGoalType.SkipAction, default, default, default);
-    public static BotGoal Move(StandingPoint destination) => new BotGoal(BotGoalType.Move, destination, default, default);
-    public static BotGoal Attack(Vector2 shootVector, Item preferredItem) => new BotGoal(BotGoalType.Attack, default, shootVector, preferredItem);
-    public static BotGoal UseItem(Item preferredItem) => new BotGoal(BotGoalType.UseItem, default, default, preferredItem);
+    public static BotGoal SkipAction() => new BotGoal(BotGoalType.SkipAction, default, default);
+    public static BotGoal Move(Vector2 jumpVector) => new BotGoal(BotGoalType.Move, jumpVector, default);
+    public static BotGoal Attack(Vector2 attackVector, Item preferredItem) => new BotGoal(BotGoalType.Attack, attackVector, preferredItem);
+    public static BotGoal UseItem(Item preferredItem) => new BotGoal(BotGoalType.UseItem, default, preferredItem);
 
-    public BotGoal(BotGoalType goal, StandingPoint destination, Vector2 attackVector, Item preferredItem)
+    public BotGoal(BotGoalType goal, Vector2 aimVector, Item preferredItem)
     {
         GoalType = goal;
-        AttackVector = attackVector;
-        Destination = destination;
+        AimVector = aimVector;
         PreferredItem = preferredItem;
     }
 }
